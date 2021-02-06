0d964dbab4bcfae27f9e2a6c1471c93990430a28
Iqbal Musyaffa <iqbalm1995@gmail.com>
2021-02-07 05:09:58 +0700

    CRUD Hak akses

--- application/controllers/admin/Hak_akses.php
+++ application/controllers/admin/Hak_akses.php
@@ -37,4 +37,223 @@ class Hak_akses extends CI_Controller {
         $data = $this->Model_hak_akses->list_hak_akses();
         return $data;
     }
+
+    public function detail_hak_akses($akses_id)
+    {
+        $data = $this->Model_hak_akses->detail_hak_akses($akses_id);
+        return $data;
+    }
+
+    public function tambah_hak_akses()
+    {
+        // judul halaman
+        $head['title']          = 'TAMBAH HAK AKSES';
+
+        $data = array(
+            'aksi'                      => 'tambah',
+            'url_proses'                => base_url('admin/hak_akses/simpan_hak_akses'),
+            'akses_id'                  => set_value('akses_id'),
+            'nama_akses'                => set_value('nama_akses'),
+            'deskripsi_akses'           => set_value('deskripsi_akses'),
+            'status_akses'              => set_value('status_akses')
+        );
+        
+        // view halaman
+        $this->load->view('backend/templates/header', $head);
+        $this->load->view('backend/templates/sidebar');
+        $this->load->view('backend/hak_akses/form_hak_akses', $data);
+        $this->load->view('backend/templates/footer');
+    }
+
+    public function ubah_hak_akses($akses_id = null)
+    {
+        if($akses_id == null){
+            $this->session->set_flashdata('message1', '
+                <div class="ui red message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Error pada saat mengambil data</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }elseif($akses_id == '1'){
+            $this->session->set_flashdata('message1', '
+                <div class="ui red message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Data Superadmin tidak bisa diubah</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }
+
+        // judul halaman
+        $head['title']          = 'UBAH HAK AKSES';
+
+        // ambil data dari fungsi detail_hak_akses
+        $get_data = $this->detail_hak_akses($akses_id);
+
+        if ($get_data) {
+
+            $data = array(
+                'aksi'                      => 'ubah',
+                'url_proses'                => base_url('admin/hak_akses/simpan_hak_akses'),
+                'akses_id'                  => set_value('akses_id', $get_data->akses_id),
+                'nama_akses'                => set_value('nama_akses', $get_data->nama_akses),
+                'deskripsi_akses'           => set_value('deskripsi_akses', $get_data->deskripsi_akses),
+                'status_akses'              => set_value('status_akses', $get_data->status_akses)
+            );
+            
+            // view halaman
+            $this->load->view('backend/templates/header', $head);
+            $this->load->view('backend/templates/sidebar');
+            $this->load->view('backend/hak_akses/form_hak_akses', $data);
+            $this->load->view('backend/templates/footer');
+
+        } else {
+
+            $this->session->set_flashdata('message1', '
+                <div class="ui positive message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Error pada saat mengambil data</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+
+        }
+    }
+
+    public function simpan_hak_akses()
+    {
+
+        $aksi_simpan = $this->input->post('aksi');
+        
+        $data = array(
+            'nama_akses'                => $this->input->post('nama_akses'),
+            'deskripsi_akses'           => $this->input->post('deskripsi_akses'),
+            'status_akses'              => $this->input->post('status_akses')
+        );
+
+        // jika aksinya tambah
+        if ($aksi_simpan == 'tambah') {
+
+            $simpan = $this->Model_hak_akses->simpan_hak_akses($data);
+
+        }elseif ($aksi_simpan == 'ubah') {
+        // jika aksinya ubah
+            $where = array('akses_id' => $this->input->post('akses_id'));
+
+            $simpan = $this->Model_hak_akses->ubah_hak_akses($where, $data);
+        }
+
+        if ($simpan) {
+            $this->session->set_flashdata('message1', '
+                <div class="ui positive message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Berhasil
+                    </div>
+                    <p>Hak akses berhasil ditambahkan.</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }else{
+            $this->session->set_flashdata('message1', '
+                <div class="ui negative message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Hak akses gagal ditambahkan.</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }
+
+    }
+
+    public function hapus_hak_akses($akses_id = null)
+    {
+        if($akses_id == null){
+            $this->session->set_flashdata('message1', '
+                <div class="ui red message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Error pada saat mengambil data</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }elseif($akses_id == '1'){
+            $this->session->set_flashdata('message1', '
+                <div class="ui red message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Data Superadmin tidak bisa dihapus</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+        }
+
+        // judul halaman
+        $head['title']          = 'UBAH HAK AKSES';
+
+        // ambil data dari fungsi detail_hak_akses
+        $get_data = $this->detail_hak_akses($akses_id);
+
+        if ($get_data) {
+
+            $hapus = $this->Model_hak_akses->hapus_hak_akses($akses_id);
+
+            if ($hapus) {
+                $this->session->set_flashdata('message1', '
+                    <div class="ui positive message">
+                        <i class="close icon"></i>
+                        <div class="header">
+                            Berhasil
+                        </div>
+                        <p>Hak akses berhasil hapus.</p>
+                    </div>
+                    ');
+                redirect (base_url('admin/hak_akses'));
+            }else{
+                $this->session->set_flashdata('message1', '
+                    <div class="ui negative message">
+                        <i class="close icon"></i>
+                        <div class="header">
+                            Gagal
+                        </div>
+                        <p>Hak akses gagal hapus.</p>
+                    </div>
+                    ');
+                redirect (base_url('admin/hak_akses'));
+            }
+
+        } else {
+
+            $this->session->set_flashdata('message1', '
+                <div class="ui positive message">
+                    <i class="close icon"></i>
+                    <div class="header">
+                        Gagal
+                    </div>
+                    <p>Error pada saat mengambil data</p>
+                </div>
+                ');
+            redirect (base_url('admin/hak_akses'));
+
+        }
+    }
+
+
 }
---
+++ application/views/backend/hak_akses/Form_hak_akses.php
@@ -0,0 +1,45 @@
+BEGIN CONTEN -->
+<div class="right floated thirteen wide computer sixteen wide phone column" id="content">
+	<div class="ui container grid">
+		<div class="row">
+			<div class="fifteen wide computer sixteen wide phone centered column">
+				<h2><i class="table icon"></i> <?= strtoupper($aksi); ?> HAK AKSES</h2>
+				<div class="ui divider"></div>
+				<div class="ui grid">
+					<div class="sixteen wide computer sixteen wide phone centered column">
+						<?php echo $this->session->userdata('message1') <> '' ? $this->session->userdata('message1') : ''; ?>	
+                        <!-- MAIN CONTENT -->
+                        <form action="<?php echo $url_proses; ?>" method="post" enctype="multipart/form-data">
+                            <div class="ui segment">
+                                <div class="ui form">
+                                    <!-- <div class="ui divider"></div> -->
+                                    <input type="hidden" name="aksi" value="<?php echo $aksi; ?>">
+                                    <input type="hidden" name="akses_id" value="<?php echo $akses_id; ?>">
+                                    <div class="field">
+                                        <label>Nama Akses</label>
+                                        <input type="text" name="nama_akses" placeholder="Isi Nama Akses..." value="<?php echo $nama_akses; ?>" required>
+                                    </div>
+                                    <div class="field">
+                                        <label>Deskripsi</label>
+                                        <textarea rows="3" name="deskripsi_akses" placeholder="Isi Deskripsi..."><?php echo $deskripsi_akses; ?></textarea>
+                                    </div>
+                                    <div class="field">
+                                        <label>Status</label>
+                                        <select name="status_akses" required>
+                                            <option value="1" <?= ( $status_akses == '1' ? 'selected' : '' ); ?>>Aktif</option>
+                                            <option value="0" <?= ( $status_akses == '0' ? 'selected' : '' ); ?>>Tidak Aktif</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <br>
+                                <button type="submit" class="ui primary button"><i class="save icon"></i>SIMPAN</button>
+                                <a class="ui button" href="<?php echo base_url('admin/hak_akses/'); ?>"><i class="cancel icon"></i>BATAL</a>
+                            </div>
+                        </form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END CONTENT
