a9101e4740af3252623a1f1acc150be553ddc8b8
Iqbal Musyaffa <iqbalm1995@gmail.com>
2021-02-07 03:43:44 +0700

    some update

--- application/controllers/admin/Hak_akses.php
+++ application/controllers/admin/Hak_akses.php
@@ -2,12 +2,39 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Hak_akses extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Set timezone
+        date_default_timezone_set('Asia/Jakarta');
+        // Load Model
+        $this->load->model('Model_hak_akses');
+        //sesion login
+        // if(($this->session->userdata('status_login') != "loginactive") && ($this->session->userdata('role') != 'admin')){
+		// 	redirect(base_url().'login');
+		// }
+        
+    }
+
 	public function index()
 	{
-		$data['title'] = 'DATA HAK AKSES';
-		$this->load->view('backend/templates/header', $data);
+        // judul halaman
+        $head['title']          = 'DATA HAK AKSES';
+
+        // ambil data dari fungsi data_hak_akses
+        $data['data_hak_akses'] = $this->data_hak_akses();
+        
+        // view halaman
+		$this->load->view('backend/templates/header', $head);
 		$this->load->view('backend/templates/sidebar');
-		$this->load->view('backend/hak_akses/view_hak_akses');
+		$this->load->view('backend/hak_akses/view_hak_akses', $data);
 		$this->load->view('backend/templates/footer');
-	}
+    }
+    
+    public function data_hak_akses()
+    {
+        $data = $this->Model_hak_akses->list_hak_akses();
+        return $data;
+    }
 }
--- application/views/backend/hak_akses/View_hak_akses.php
+++ application/views/backend/hak_akses/View_hak_akses.php
@@ -21,84 +21,49 @@
 						<div class="ui stacked segment">
 							<div class="ui blue ribbon icon label">DATATABLE SUPPORT</div>
 							<br><br>
+							<div class="ui clearing segment">
+								<a href="<?php echo base_url('admin/hak_akses/tambah_hak_akses/'); ?>" class="ui right floated primary button"><i class="plus icon"></i>Tambah Hak Akses</a>
+							</div>
+							<br>
 							<table id="example" class="ui celled table responsive nowrap unstackable" style="width:100%">
 							    <thead>
 							        <tr>
-							            <th>First name</th>
-							            <th>Last name</th>
-							            <th>Position</th>
-							            <th>Office</th>
-							            <th>Age</th>
-							            <th>Start date</th>
-							            <th>Salary</th>
-							            <th>Extn.</th>
-							            <th>E-mail</th>
+							            <th>#</th>
+							            <th>Nama Akses</th>
+							            <th>Deskripsi Akses</th>
+							            <th>Status Akses</th>
+							            <th>Aksi</th>
 							        </tr>
 							    </thead>
 							    <tbody>
-							        <tr>
-							            <td>Tiger</td>
-							            <td>Nixon</td>
-							            <td>System Architect</td>
-							            <td>Edinburgh</td>
-							            <td>61</td>
-							            <td>2011/04/25</td>
-							            <td>$320,800</td>
-							            <td>5421</td>
-							            <td>t.nixon@datatables.net</td>
-							        </tr>
-							        <tr>
-							            <td>Garrett</td>
-							            <td>Winters</td>
-							            <td>Accountant</td>
-							            <td>Tokyo</td>
-							            <td>63</td>
-							            <td>2011/07/25</td>
-							            <td>$170,750</td>
-							            <td>8422</td>
-							            <td>g.winters@datatables.net</td>
-							        </tr>
-							        <tr>
-							            <td>Ashton</td>
-							            <td>Cox</td>
-							            <td>Junior Technical Author</td>
-							            <td>San Francisco</td>
-							            <td>66</td>
-							            <td>2009/01/12</td>
-							            <td>$86,000</td>
-							            <td>1562</td>
-							            <td>a.cox@datatables.net</td>
-							        </tr>
-							        <tr>
-							            <td>Cedric</td>
-							            <td>Kelly</td>
-							            <td>Senior Javascript Developer</td>
-							            <td>Edinburgh</td>
-							            <td>22</td>
-							            <td>2012/03/29</td>
-							            <td>$433,060</td>
-							            <td>6224</td>
-							            <td>c.kelly@datatables.net</td>
-							        </tr>
-							        <tr>
-							            <td>Airi</td>
-									            <td>Satou</td>
-									            <td>Accountant</td>
-									            <td>Tokyo</td>
-									            <td>33</td>
-									            <td>2008/11/28</td>
-									            <td>$162,700</td>
-									            <td>5407</td>
-									            <td>a.satou@datatables.net</td>
-									        </tr>
-									    </tbody>
-									</table>
-								</div>
-								<!-- END DATATABLE -->
-							</div>
+							    	<?php
+							    		$no = 1;
+							    		// jika data kosong
+							    		if (empty($data_hak_akses)) { ?>	                                    <tr>
+	                                        <td colspan = "5" class="warning center aligned">
+	                                            <i class="attention icon"></i> Data Belum Ada
+	                                        </td>
+	                                    </tr>
+							    	<?php }else{ 
+							    		foreach ($data_hak_akses as $r) { ?>
+								        <tr>
+								            <td class="center aligned"><?= $no++; ?></td>
+								            <td><?= $r->nama_akses; ?></td>
+								            <td><?= $r->deskripsi_akses; ?></td>
+								            <td class="center aligned"><?= $r->status_akses; ?></td>
+								            <td class="center aligned">-</td>
+								        </tr>
+							    	<?php } /*end foreach*/ } /*end if*/ ?>
+							        
+							                
+								</tbody>
+							</table>
 						</div>
+						<!-- END DATATABLE -->
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
+</div>
 		<!-- END CONTENT -->
